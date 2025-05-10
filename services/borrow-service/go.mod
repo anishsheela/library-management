367601504b01f9782829d2e1b690b62018fa6098
Anish Sheela <aneesh.nl@gmail.com>
2025-05-10 17:22:07 +0530

    Update the Kafka library

--- services/borrow-service/go.mod
+++ services/borrow-service/go.mod
@@ -4,16 +4,21 @@ go 1.24.2
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
-	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/go-sql-driver/mysql v1.9.2
 	github.com/gorilla/mux v1.8.1
-	github.com/stretchr/testify v1.7.1
+	github.com/segmentio/kafka-go v0.4.47
+	github.com/stretchr/testify v1.8.0
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- services/borrow-service/internal/db/kafka.go
+++ services/borrow-service/internal/db/kafka.go
@@ -1,34 +1,41 @@
 package db
 
 import (
-    "fmt"
+	"context"
+	"fmt"
 	"os"
-    "github.com/confluentinc/confluent-kafka-go/kafka"
+	"time"
+
+	"github.com/segmentio/kafka-go"
 )
 
-var producer *kafka.Producer
+var writer *kafka.Writer
 
 func InitKafkaProducer() {
-    var err error
-    producer, err = kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": os.Getenv("KAFKA_BROKER"),
-		"sasl.username": os.Getenv("KAFKA_USERNAME"),
-		"sasl.password": os.Getenv("KAFKA_PASSWORD"),
-	})
-    if err != nil {
-        fmt.Printf("Failed to create producer: %s\n", err)
-    }
+	broker := os.Getenv("KAFKA_BROKER")
+	topic := os.Getenv("KAFKA_TOPIC") // Optional: can be passed dynamically to PublishEvent instead
+
+	writer = &kafka.Writer{
+		Addr:         kafka.TCP(broker),
+		Topic:        topic,
+		Balancer:     &kafka.LeastBytes{},
+		RequiredAcks: kafka.RequireAll,
+		Async:        false,
+	}
+	fmt.Println("Kafka producer initialized.")
 }
 
 func PublishEvent(topic string, message string) {
-    err := producer.Produce(&kafka.Message{
-        TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-        Value:          []byte(message),
-    }, nil)
-    if err != nil {
-        fmt.Printf("Error producing message to %s: %s\n", topic, err)
-    } else {
-        fmt.Printf("Message successfully produced to %s!\n", topic)
-    }
+	err := writer.WriteMessages(context.Background(),
+		kafka.Message{
+			Topic: topic,
+			Value: []byte(message),
+			Time:  time.Now(),
+		},
+	)
+	if err != nil {
+		fmt.Printf("Error producing message to %s: %s\n", topic, err)
+	} else {
+		fmt.Printf("Message successfully produced to %s!\n", topic)
+	}
 }
-
